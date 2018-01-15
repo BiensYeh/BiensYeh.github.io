2f1af6e63d66ba32bdcfa2b1f69522580eca4f50
francois <f.aubry@uclouvain.be>
2018-01-16 08:10:18 +1300

    up

--- data.js
+++ data.js
@@ -2,9 +2,7 @@ var BLUE = "#00BFFF";
 var RED = "#FF6347";
 var BLACK = "#696969";
 var NEUTRAL = "#FFF8DC";
-
 var font = "Bookman";
-
 var width = 200;
 var height = 100;
 var margin = 10;
@@ -13,15 +11,107 @@ var nbBlue = 8;
 var nbRed = 7;
 var frameImg = createImage("frame.png");
 
-var secretTable = makeSecretTable();
+function onIndexLoad() {
+  console.log("load");
+  makeSecretTable(); 
+  makeWordTable();
+  console.log(getCookie("color_0_0"));
+}
 
-var wordTable = makeWordTable();
-var cardCanvas = makeMatrix(n, n, null);
-var fontMatrix = makeMatrix(n, n, null);
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-for(var i = 0; i < wordTable.length; i++) {
-  for(var j = 0; j < wordTable[i].length; j++) {
-    makeCanvas(i, j);
+function randint(lb, ub) {
+  return Math.floor((Math.random() * ub) + lb);
+}
+
+function makeMatrix(nbRows, nbCols, value) {
+  var array = new Array(nbRows);
+  for(var i = 0; i < nbRows; i++) {
+    array[i] = new Array(nbCols);
+    for(var j = 0; j < nbCols; j++) {
+      array[i][j] = value;
+    }
+  }
+  return array;
+}
+
+function makeSecretTable() {
+  var table = makeMatrix(n, n, NEUTRAL);
+  var blue = new Set();
+  while(blue.size < nbBlue) {
+    blue.add(randint(0, 24));
+  }
+  var red = new Set();
+  while(red.size < nbRed) {
+    var i = randint(0, 24);
+    if(!blue.has(i)) {
+      red.add(i);
+    }
+  }
+  var black = randint(0, 24);
+  while(red.has(black) || blue.has(black)) {
+    black = randint(0, 24);
+  }
+  var row = Math.floor(black / n);
+  var col = black % n;
+  table[row][col] = BLACK;
+  blue = Array.from(blue);
+  for(var i = 0; i < blue.length; i++) {
+    var row = Math.floor(blue[i] / n);
+    var col = blue[i] % n;
+    table[row][col] = BLUE;
+  }
+  red = Array.from(red);
+  for(var i = 0; i < red.length; i++) {
+    var row = Math.floor(red[i] / n);
+    var col = red[i] % n;
+    table[row][col] = RED;
+  }
+  for(var i = 0; i < n; i++) {
+    for(var j = 0; j < n; j++) {
+      setCookie("color_" + i + "_" + j, table[i][j], 1);
+    }
   }
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function createImage(imgSrc) {
+  var img = new Image();
+  img.src = imgSrc;
+  return img;
+}
+
+function makeWordTable() {
+  var table = makeMatrix(n, n, "N");
+  var indexes = new Set();
+  while(indexes.size < n * n) {
+    indexes.add(randint(0, words.length - 1));
+  }
+  indexes = Array.from(indexes);
+  for(var i = 0; i < indexes.length; i++) {
+    var row = Math.floor(i / n);
+    var col = i % n;
+    table[row][col] = words[indexes[i]];
+    setCookie("word_" + row + "_" + col, table[row][col], 1);
+  }
+}
--- game.js
+++ game.js
@@ -1,73 +1,59 @@
-function randint(lb, ub) {
-  return Math.floor((Math.random() * ub) + lb);
-}
 
-function makeMatrix(nbRows, nbCols, value) {
-  var array = new Array(nbRows);
-  for(var i = 0; i < nbRows; i++) {
-    array[i] = new Array(nbCols);
-    for(var j = 0; j < nbCols; j++) {
-      array[i][j] = value;
+var fontMatrix = makeMatrix(n, n, null);
+
+function onPrivateBodyLoad() {
+  console.log("private");
+  for(var i = 0; i < n; i++) {
+    for(var j = 0; j < n; j++) {
+      makeCanvas(i, j, true);
     }
   }
-  return array;
 }
 
-function makeSecretTable() {
-  var table = makeMatrix(n, n, NEUTRAL);
-  var blue = new Set();
-  while(blue.size < nbBlue) {
-    blue.add(randint(0, 24));
-  }
-  var red = new Set();
-  while(red.size < nbRed) {
-    var i = randint(0, 24);
-    if(!blue.has(i)) {
-      red.add(i);
+function onPublicBodyLoad() {
+  console.log("public");
+  for(var i = 0; i < n; i++) {
+    for(var j = 0; j < n; j++) {
+      makeCanvas(i, j, false);
     }
   }
-  var black = randint(0, 24);
-  while(red.has(black) || blue.has(black)) {
-    black = randint(0, 24);
-  }
-  var row = Math.floor(black / n);
-  var col = black % n;
-  table[row][col] = BLACK;
-  blue = Array.from(blue);
-  for(var i = 0; i < blue.length; i++) {
-    var row = Math.floor(blue[i] / n);
-    var col = blue[i] % n;
-    table[row][col] = BLUE;
-  }
-  red = Array.from(red);
-  for(var i = 0; i < red.length; i++) {
-    var row = Math.floor(red[i] / n);
-    var col = red[i] % n;
-    table[row][col] = RED;
-  }
-  return table;
 }
 
-function createImage(imgSrc) {
-  var img = new Image();
-  img.src = imgSrc;
-  return img;
+function deleteById(id) {
+  var element = document.getElementById(id);
+  if(element != null) {
+    element.outerHTML = "";
+    delete element;
+  }
 }
 
-function makeWordTable() {
-  var table = makeMatrix(n, n, "N");
-  var indexes = new Set();
-  while(indexes.size < n * n) {
-    indexes.add(randint(0, words.length - 1));
-  }
-  indexes = Array.from(indexes);
-  for(var i = 0; i < indexes.length; i++) {
-    var row = Math.floor(i / n);
-    var col = i % n;
-    table[row][col] = words[indexes[i]];
-  }
-  return table;
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
 
 function clearCanvas(canvas) {
   var context = canvas.getContext("2d");
@@ -81,129 +67,53 @@ function fill(canvas, color) {
   context.fillRect(11, 8, 188 - 11, 90 - 8);
 }
 
-function makeCanvas(row, col) {
+function getWord(row, col) {
+  return getCookie("word_" + row + "_" + col);
+}
+
+function getColor(row, col) {
+  return getCookie("color_" + row + "_" + col);
+}
+
+function makeCanvas(row, col, private) {
   var canvas = document.createElement("canvas");
   canvas.width = width;
   canvas.height = height;
   var context = canvas.getContext("2d");
-
-  
-  //fill(canvas, secretTable[row][col]);
-
   var size = 30;
   context.font = size + "px " + font;
-  while(context.measureText(wordTable[row][col]).width > 160) {
+  while(context.measureText(getWord(row, col)).width > 160) {
     size -= 1;
     context.font = size + "px " + font;
   }
 
+  if(private) {
+    fill(canvas, getColor(row, col));
+  }
+
   fontMatrix[row][col] = size + "px " + font;
 
-  var textWidth = context.measureText(wordTable[row][col]).width;
+  var textWidth = context.measureText(getWord(row, col)).width;
   //context.fillRect(0, 0, width, height);
   context.drawImage(frameImg, 0, 0);
   context.fillStyle = "#000000";
-  context.fillText(wordTable[row][col], (width - textWidth) / 2, (height + 10) / 2);
+  context.fillText(getWord(row, col), (width - textWidth) / 2, (height + 10) / 2);
   var x = (width + margin) * col + (screen.width - 50 - n * (width + margin)) / 2;
   var y = (height + margin) * row + (screen.height - 200 - n * (height + margin)) / 2;
   canvas.style = "position: fixed; left: " + x + "px; top: " + y + "px;";
 
-  canvas.addEventListener("click", function() {
-    clearCanvas(canvas);
-    fill(canvas, secretTable[row][col]);
-    context.drawImage(frameImg, 0, 0);
-    context.font = fontMatrix[row][col];
-    var textWidth = context.measureText(wordTable[row][col]).width;
-    console.log(wordTable[row][col]);
-    context.fillStyle = "#000000";
-    context.fillText(wordTable[row][col], (width - textWidth) / 2, (height + 10) / 2);
-  }, false);
+  if(private) {
+    canvas.addEventListener("click", function() {
+      clearCanvas(canvas);
+      fill(canvas, secretTable[row][col]);
+      context.drawImage(frameImg, 0, 0);
+      context.font = fontMatrix[row][col];
+      var textWidth = context.measureText(wordTable[row][col]).width;
+      console.log(wordTable[row][col]);
+      context.fillStyle = "#000000";
+      context.fillText(wordTable[row][col], (width - textWidth) / 2, (height + 10) / 2);
+    }, false);
+  }
 
-  cardCanvas[row][col] = canvas;
-
-  document.getElementById("test").appendChild(canvas);
+  document.getElementById("container").appendChild(canvas);
 }
-
-  
-/*
-import random
-
-class Word:
-
-  def __init__(self, fr, en):
-    self.fr = fr
-    self.en = en
-
-  def __str__(self):
-    return '{0} {1}'.format(self.fr, self.en)
-
-def upfirst(s):
-  return s[0].upper() + s[1:]
-
-def read_words():
-  f = open('words', 'r')
-  lines = [line.strip() for line in f.readlines()]
-  f.close()
-  words = [ ]
-  for line in lines:
-    data = line.split(' ')
-    fr = upfirst(data[0])
-    en = upfirst(data[1])
-    words.append(Word(fr, en))
-  return words
-
-def get_pos(i):
-  return (i / 5, i % 5)
-
-def make_word_matrix(words):
-  indexes = set()
-  while len(indexes) < 25:
-    indexes.add(random.randint(0, len(words) - 1))
-  print(indexes)
-  M = [ [ ''  for j in range(5) ] for i in range(5) ]
-  k = 0
-  for i in indexes:
-    x, y = get_pos(k)
-    k += 1
-    M[x][y] = words[i]
-  return M
-
-def make_secret_matrix():
-  M = [ [ 'Neutral' for j in range(5) ] for i in range(5) ]
-  blue = set()
-  red = set()
-  while len(blue) < 8:
-    i = random.randint(0, 24)
-    blue.add(i)
-  while len(red) < 7:
-    i = random.randint(0, 24)
-    if not i in blue:
-      red.add(i)
-  i = random.randint(0, 24)
-  while i in blue or i in red:
-    i = random.randint(0, 24)
-  black = i
-  for i in blue:
-    x, y = get_pos(i)
-    M[x][y] = 'Blue'
-  for i in red:
-    x, y = get_pos(i)
-    M[x][y] = 'Red'
-  x, y = get_pos(black)
-  M[x][y] = 'Black'
-  return M
-
-words = read_words()
-
-secret = make_secret_matrix()
-for l in secret:
-  print(l)
-
-matrix = make_word_matrix(words)
-s = ''
-for i in range(5):
-  for j in range(5):
-    s += str(matrix[i][j]) + '\t'
-  s += '\n'
-print(s)
-*/
